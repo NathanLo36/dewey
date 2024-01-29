39b2d32070c1c351bea28cf40577dd279c812bb3
Nathan Lo <natefung2012@gmail.com>
2024-01-29 12:59:23 -0500

    chore: progress sync

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ def main():
     print("Main called")
 
 if __name__ == "__main__":
-    p = Path('.').resolve()
-    print(p)
+    working_dir = input("Enter directory for sorting: ")
+    filter_file = input("Enter filter file path: ")
+
+
+    fs1 = fs.Filesorter(filter_file, working_dir)
+    fs1.filter_file_list_check()
+    fs1.resolve_moves()
+    fs1.list_conflicts()
