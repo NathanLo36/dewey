254dca669dd119b4cfeccec4055e9bdace854af3
Nathan Lo <natefung2012@gmail.com>
2024-02-07 18:10:46 -0500

    feat: added MoveAction and Conflict dataclasses and refactored accordingly

--- filesorter.py
+++ filesorter.py
@@ -3,24 +3,31 @@ from pathlib import Path
 from dataclasses import dataclass
 
 
-@dataclass(init = True)
+@dataclass(init=True)
 class Filter:
     keywords: list[str]
     folder: Path
 
-@dataclass(init = True)
-class Move:
-    from_path: Path
+
+@dataclass(init=True)
+class MoveAction:
+    file_path: Path
     to_path: Path
+
+
+@dataclass(init=True)
+class Conflict:
+    file_path: Path
+    to_paths: list[Path]
 
 
 class Filesorter:
     def __init__(self, filter_file_path: str, working_dir: str):
         self._working_dir: Path = Path(working_dir)
         self._filter_file: Path = Path(filter_file_path)
-        self._filters: list[Filter] = extract_filters(self._filter_file)
-        self._conflicts: list[tuple[Path, list[Path]]] = []
-        self._unresolved_moves: list[tuple[Path, Path]] = []
+        self._filters: list[Filter] = self.extract_filters(self._filter_file)
+        self._conflicts: list[Conflict] = []
+        self._unresolved_moves: list[MoveAction] = []
 
     def sort(self) -> None:
         for file in self._working_dir.glob("*.*"):
@@ -30,21 +37,20 @@ class Filesorter:
                     matching_folders.append(filter.folder)
 
             if len(matching_folders) == 1:
-                self._unresolved_moves.append((file, matching_folders[0]))
+                self._unresolved_moves.append(MoveAction(file, matching_folders[0]))
             elif len(matching_folders) > 1:
-                self._conflicts.append((file, matching_folders))
+                self._conflicts.append(Conflict(file, matching_folders))
 
     def resolve_moves(self) -> None:
         for action in self._unresolved_moves:
-            move_file(action[0], action[1])
+            self.move_file(action)
 
     def list_conflicts(self) -> None:
         print("Unresolved conflicts:")
         for action in self._conflicts:
-            
-            print(f"{action[0]} -> ", end='')
-            for folder in action[1]:
-                print(f"[{folder}]", end=' ')
+            print(f"{action.file_path} -> ", end="")
+            for folder in action.to_paths:
+                print(f"[{folder}]", end=" ")
             print("\n")
 
     def filter_check(self, file_path: Path, filter: Filter) -> bool:
@@ -54,13 +60,13 @@ class Filesorter:
             if keyword in file_path.stem:
                 return True
         return False
-    
+
     @property
-    def unresolved_moves(self) -> list[Path, Path]:
+    def unresolved_moves(self) -> list[MoveAction]:
         return self._unresolved_moves
 
     @property
-    def conflicts(self) -> list[Path, list[Path]]:
+    def conflicts(self) -> list[Conflict]:
         return self._conflicts
 
     @property
@@ -78,29 +84,28 @@ class Filesorter:
     @filter_file.setter
     def filter_file(self, filter_file) -> None:
         self._filter_file = filter_file
-        self._filters = extract_filters(filter_file)
+        self._filters = self.extract_filters(filter_file)
+    
+    def move_file(self, action: MoveAction):
+        move(action.file_path, action.to_path)
+
+    def extract_filters(self, filter_file: Path) -> list[Filter]:
+        filter_list = []
+        with open(filter_file, "r") as filters:
+            for filter in filters:
+                filter_list.append(self.extract_filter(filter))
+        return filter_list
+
+    def extract_filter(self, filter: str) -> Filter:
+        content = filter.strip().split("|||")
+
+        if len(content) != 2:
+            print("Error in filter format encountered, filter extraction cancelled.")
+            return []
+
+        keywords = content[0].split(",")
+        folder = Path(content[1]).resolve()
+        return Filter(keywords, folder)
 
 
-def extract_filters(filter_file: Path) -> list[Filter]:
-    filter_list = []
-    with open(filter_file, "r") as filters:
-        for filter in filters:
-            filter_list.append(extract_filter(filter))
-    return filter_list
 
-
-def extract_filter(filter: str) -> Filter:
-    content = filter.strip().split("|||")
-
-    if len(content) != 2:
-        print("Error in filter format encountered, filter extraction cancelled.")
-        return []
-
-    keywords = content[0].split(",")
-    folder = Path(content[1]).resolve()
-    return Filter(keywords, folder)
-
-
-def move_file(fromloc: Path, toloc: Path):
-    move(fromloc, toloc)
-    print(f"File moved from {fromloc} to {toloc}")
